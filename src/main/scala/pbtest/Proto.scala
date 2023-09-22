44e3b5c9c36f2509a26857b3d4105a4999b9f1d1
James Reddick <james.reddick@pirum.com>
2023-09-22 17:48:19 +0100

    Changes

--- src/main/scala/pbtest/Proto.scala
+++ src/main/scala/pbtest/Proto.scala
@@ -2,27 +2,49 @@ package pbtest
 
 import java.io.FileOutputStream
 import java.io.FileInputStream
+import scala.jdk.CollectionConverters.*
+import pbtest.Person.PhoneNumber
 
 @main def hello() =
 
-  val personBuilder = Person.newBuilder().nn
-  personBuilder.setId(123)
-  personBuilder.setName("James")
-  personBuilder.setEmail("reddicj@gmail.com")
-  val phoneBuilder = Person.PhoneNumber.newBuilder().nn 
-  phoneBuilder.setType(Person.PhoneType.MOBILE)
-  phoneBuilder.setNumber("999")
-  personBuilder.addPhones(phoneBuilder)
-  val person = personBuilder.build().nn
+  val person      = createPerson(123, "James", "reddicj@gmail.com", List("999"))
+  val person2     = createPerson(456, "Tom", "tom@yahoo.com", List("667"))
+  val addressBook = createAddressBook(List(person, person2))
 
-  println("Write person data as pb to test.bin")
+  println("Write data as pb to test.bin")
   val fos = new FileOutputStream("test.bin")
   person.writeTo(fos)
+  // person.writeTo(fos)
+  // person2.writeDelimitedTo(fos)
   fos.close()
-  println(person.toString())
 
-  println("Read person data as pb from test.bin")
-  val fis  = new FileInputStream("test.bin")
-  val read = Person.parseFrom(fis).nn
-  fis.close()
-  println(read.toString())
+  // println("Read data as pb from test.bin")
+  // val fis  = new FileInputStream("test.bin")
+  // val read = AddressBook.parseFrom(fis)
+  // fis.close()
+  // println(read.toString())
+
+def createAddressBook(people: List[Person]): AddressBook =
+  val addressBookBuilder = AddressBook.newBuilder()
+  addressBookBuilder.addAllPeople(people.asJava)
+  addressBookBuilder.build()
+
+def createPerson(
+  id: Int,
+  name: String,
+  email: String,
+  phoneNumbers: List[String]
+): Person =
+
+  def createPhone(phoneNumber: String): PhoneNumber =
+    val phoneBuilder = Person.PhoneNumber.newBuilder()
+    phoneBuilder.setType(Person.PhoneType.MOBILE)
+    phoneBuilder.setNumber(phoneNumber)
+    phoneBuilder.build()
+
+  val personBuilder = Person.newBuilder()
+  personBuilder.setId(id)
+  personBuilder.setName(name)
+  personBuilder.setEmail(email)
+  phoneNumbers.foreach(x => personBuilder.addPhones(createPhone(x)))
+  personBuilder.build()
